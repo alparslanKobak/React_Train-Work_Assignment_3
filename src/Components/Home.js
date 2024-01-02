17f28561bd4947ff0afaeff0d2166c006dbba7c2
alparslanKobak <alparslankobak@gmail.com>
2024-01-02 15:58:04 +0300

    slide

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -95,8 +95,7 @@ function Home() {
 
 
 
-            <section
-                id="forecasts"
+            <section id="forecasts"
                 className="ds section-forecast s-overlay s-pt-90 s-pb-30 s-pt-xl-150 s-pb-xl-90 c-gutter-60 c-mb-60"
             >
                 <div className="container">
@@ -321,6 +320,184 @@ function Home() {
                     </div>
                 </div>
             </section>
+
+
+
+            <section
+  id="services"
+  className="ds section-service s-overlay mobile-overlay s-py-90 s-py-xl-150 container-px-xl-0"
+>
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-xl-5" />
+      <div className="col-xl-3 col-lg-6">
+        <p className="text-white text-center text-md-left fs-20 line-right mb-0">
+          <span>Services</span>
+        </p>
+        <h3 className="special-heading mt-2 fs-60 text-center text-md-left">
+          <span>
+            Let’s Make <br />
+            Some Magic
+          </span>
+        </h3>
+      </div>
+      <div className="col-xl-3 col-lg-6 text-center text-md-left">
+        <p className="text-center text-md-left  mt-4 mt-lg-0">
+          <span>
+            They will provide the best free horoscope <br />
+            astrology to you by analysing your sign.
+          </span>
+        </p>
+        <div className="divider-35" />
+        <a href="contact.html" className="btn btn-outline-maincolor btn-medium">
+          Book Reading Now
+        </a>
+      </div>
+      <div className="col-xl-1" />
+      <div className="col-xl-3" />
+      <div className="col-xl-9">
+        <div className="big-width">
+          <div className="divider-55" />
+          <div
+            className="owl-carousel "
+            data-responsive-lg={4}
+            data-responsive-md={3}
+            data-responsive-sm={2}
+            data-responsive-xs={1}
+            data-nav="false"
+            data-margin={30}
+            data-loop="true"
+            data-autoplay="true"
+          >
+            <div className="vertical-item service-layout-2 content-absolute box-shadow  content-padding">
+              <div className="item-media">
+                <img src="assets/images/services/img_8.jpg" alt="img" />
+                <div className="media-links">
+                  <a className="abs-link" title="" href="service-single.html" />
+                </div>
+              </div>
+              <div className="item-content overflow-hidden">
+                <div className="d-flex align-center flex-wrap">
+                  <h5 className="my-0 links-maincolor2 mr-auto pr-2">
+                    <a href="service-single.html">Natal Chart</a>
+                  </h5>
+                  <a href="service-single.html" className="font-2">
+                    Read Now <i className="fa fa-caret-right color-main ml-2" />
+                  </a>
+                </div>
+                <p>Integer sagittis nisi nec tortor fermentum aliquet.</p>
+              </div>
+            </div>
+            <div className="vertical-item service-layout-2 content-absolute box-shadow  content-padding">
+              <div className="item-media">
+                <img src="assets/images/services/img_9.jpg" alt="img" />
+                <div className="media-links">
+                  <a className="abs-link" title="" href="service-single.html" />
+                </div>
+              </div>
+              <div className="item-content overflow-hidden">
+                <div className="d-flex align-center flex-wrap">
+                  <h5 className="my-0 links-maincolor2 mr-auto pr-2">
+                    <a href="service-single.html">Transit Chart</a>
+                  </h5>
+                  <a href="service-single.html" className="font-2">
+                    Read Now <i className="fa fa-caret-right color-main ml-2" />
+                  </a>
+                </div>
+                <p>Integer sagittis nisi nec tortor fermentum aliquet.</p>
+              </div>
+            </div>
+            <div className="vertical-item service-layout-2 content-absolute box-shadow  content-padding">
+              <div className="item-media">
+                <img src="assets/images/services/img_10.jpg" alt="img" />
+                <div className="media-links">
+                  <a className="abs-link" title="" href="service-single.html" />
+                </div>
+              </div>
+              <div className="item-content overflow-hidden">
+                <div className="d-flex align-center flex-wrap">
+                  <h5 className="my-0 links-maincolor2 mr-auto pr-2">
+                    <a href="service-single.html">Birth Stone</a>
+                  </h5>
+                  <a href="service-single.html" className="font-2">
+                    Read Now <i className="fa fa-caret-right color-main ml-2" />
+                  </a>
+                </div>
+                <p>Integer sagittis nisi nec tortor fermentum aliquet.</p>
+              </div>
+            </div>
+            <div className="vertical-item service-layout-2 content-absolute box-shadow  content-padding">
+              <div className="item-media">
+                <img src="assets/images/services/img_11.jpg" alt="img" />
+                <div className="media-links">
+                  <a className="abs-link" title="" href="service-single.html" />
+                </div>
+              </div>
+              <div className="item-content overflow-hidden">
+                <div className="d-flex align-center flex-wrap">
+                  <h5 className="my-0 links-maincolor2 mr-auto pr-2">
+                    <a href="service-single.html">Medical Reading</a>
+                  </h5>
+                  <a href="service-single.html" className="font-2">
+                    Read Now <i className="fa fa-caret-right color-main ml-2" />
+                  </a>
+                </div>
+                <p>Integer sagittis nisi nec tortor fermentum aliquet.</p>
+              </div>
+            </div>
+            <div className="vertical-item service-layout-2 content-absolute box-shadow  content-padding">
+              <div className="item-media">
+                <img src="assets/images/services/img_12.jpg" alt="img" />
+                <div className="media-links">
+                  <a className="abs-link" title="" href="service-single.html" />
+                </div>
+              </div>
+              <div className="item-content overflow-hidden">
+                <div className="d-flex align-center flex-wrap">
+                  <h5 className="my-0 links-maincolor2 mr-auto pr-2">
+                    <a href="service-single.html">Birth Stone</a>
+                  </h5>
+                  <a href="service-single.html" className="font-2">
+                    Read Now <i className="fa fa-caret-right color-main ml-2" />
+                  </a>
+                </div>
+                <p>Integer sagittis nisi nec tortor fermentum aliquet.</p>
+              </div>
+            </div>
+            <div className="vertical-item service-layout-2 content-absolute box-shadow  content-padding">
+              <div className="item-media">
+                <img src="assets/images/services/img_13.jpg" alt="img" />
+                <div className="media-links">
+                  <a className="abs-link" title="" href="service-single.html" />
+                </div>
+              </div>
+              <div className="item-content overflow-hidden">
+                <div className="d-flex align-center flex-wrap">
+                  <h5 className="my-0 links-maincolor2 mr-auto pr-2">
+                    <a href="service-single.html">Transit Chart</a>
+                  </h5>
+                  <a href="service-single.html" className="font-2">
+                    Read Now <i className="fa fa-caret-right color-main ml-2" />
+                  </a>
+                </div>
+                <p>Integer sagittis nisi nec tortor fermentum aliquet.</p>
+              </div>
+            </div>
+          </div>
+          <div className="divider-35" />
+          <div className="owl-custom-nav romb">
+            <a href="#" className="owl-prev">
+              <i className="ico ico-right-arrow" />
+            </a>
+            <a href="#" className="owl-next">
+              <i className="ico ico-right-arrow" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 
         </>
