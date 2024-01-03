cfe6b8aaaa58478ac0d89589672afbecb1c7a613
alparslanKobak <alparslankobak@gmail.com>
2024-01-03 14:47:10 +0300

    .

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import AboutHome from "./AboutHome";
+import ServicesAbout from "./ServicesAbout";
 
 
 
@@ -6,6 +7,8 @@ function About() {
     return ( 
         <>
         <AboutHome />
+
+        <ServicesAbout />
         
         
         </>
